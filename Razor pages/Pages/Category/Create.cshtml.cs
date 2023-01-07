0d46211e218d77ae1e932129a1346229b2ea074e
Aswathaman M <aswath.adit@gmail.com>
2023-01-07 10:47:09 +0530

    Add Category.

--- Razor pages/Pages/Category/Create.cshtml.cs
+++ Razor pages/Pages/Category/Create.cshtml.cs
@@ -1,15 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor_pages.Data;
 using Razor_pages.Model;
+using System.ComponentModel.DataAnnotations;
 
 namespace Razor_pages.Pages.Category
 {
     public class CreateModel : PageModel
     {
-        public RCategory category { get; set; } 
+        private readonly RApplicationDbContext context;
+
+        public CreateModel(RApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public RCategory Category { get; set; } 
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost(RCategory category)
+        {
+            if (category == null)
+            {
+                throw new ValidationException();
+            }
+
+            await context.AddAsync(category);
+            context.SaveChangesAsync();
+
+            return RedirectToPage("RCategory");
         }
     }
 }
